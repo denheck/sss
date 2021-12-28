5e25565ebee77d19e85da1550d3fc8f2ae747fd3
Dennis Heckman <denheck@gmail.com>
2021-12-27 22:01:20 -0600

    initial project skeleton

---
+++ lib/sss.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sss/version"
+
+module Sss
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sss/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Sss
+  VERSION = "0.1.0"
+end
