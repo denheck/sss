6e70d2807fab216bfd9e71ebf24466f8b0fce0ef
Dennis Heckman <denheck@gmail.com>
2021-12-28 18:01:15 -0600

    initial executable

--- lib/sss.rb
+++ lib/sss.rb
@@ -1,8 +1,88 @@
 # frozen_string_literal: true
 
+require "paint"
+require "thor"
+
+require_relative "sss/installer"
 require_relative "sss/version"
 
 module Sss
+  SCRIPTS_DIR = File.join(Dir.home, ".ws", "scripts")
+
+  class SetupParser
+    attr_reader :installers
+  
+    def initialize
+      @installers = []
+    end
+  
+    def snap(package_name, *opts)
+      @installers << Installer::SnapPackage.new(package_name, opts)
+    end
+  
+    def apt(package_name)
+      @installers << Installer::AptPackage.new(package_name)
+    end
+  
+    def mkdir(dir)
+      @installers << Installer::Directory.new(dir)
+    end
+  
+    def script(source)
+      @installers << Installer::Script.new(source)
+    end
+  
+    def new_group(name, *opts)
+      @installers << Installer::NewGroup.new(name, opts)
+    end
+  
+    def new_group_member(user, group)
+      @installers << Installer::NewGroupMember.new(user, group)
+    end
+
+    def git_conf(key, value)
+      @installers << Installer::GitConf.new(key, value)
+    end
+
+    def bashrc(code)
+      @installers << Installer::BashRC.new(code)
+    end
+
+    def bin(source, name = nil)
+      @installers << Installer::Bin.new(source, name)
+    end
+
+    def gsettings(schema, key, value)
+      @installers << Installer::Gsettings.new(schema, key, value)
+    end
+  end
+
+  class Runner < Thor
+    def self.exit_on_failure?
+      true
+    end
+
+    desc "setup MANIFEST", "Setup the workstation from a manifest file"
+    def setup(manifest)
+      # initialize environment
+      FileUtils.mkdir_p(Sss::SCRIPTS_DIR)
+
+      # parse setup file
+      contents = File.read(manifest)
+      parser = Sss::SetupParser.new
+      parser.instance_eval(contents)
+
+      # run installers
+      parser.installers.each do |installer|
+        if installer.installed?
+          puts("#{Paint["(skip)", :green]} #{installer.class.name} '#{installer}' already installed")
+        else
+          puts("#{Paint["(install)", :green]} Installing #{installer.class.name} '#{installer}'")
+          installer.install  
+        end
+      end
+    end
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ lib/sss/installer.rb
@@ -0,0 +1,307 @@
+require "digest/sha1"
+require "fileutils"
+require "open-uri"
+require "uri"
+
+module Sss
+  module Installer
+    class SnapPackage
+        def initialize(name, options)
+          @name = name
+          @install_flags = options.select{ |option| option == :classic }.map{ |option| "--#{option.to_s}" }
+        end
+      
+        def to_s
+          @name
+        end
+      
+        def installed?
+          package_rows = %x(snap list)
+      
+          package_rows.split("\n").find{ |package_row| package_row.split(" ")[0] == @name }
+        end
+      
+        def install
+          IO.popen("snap install #{@name} #{@install_flags * " "}") do |io|
+            while (line = io.gets) do
+              puts(line)
+            end
+          end
+        end
+      end
+      
+      class AptPackage
+        def initialize(name)
+          @name = name
+        end
+      
+        def to_s
+          @name
+        end
+      
+        def installed?
+          %x(dpkg -l #{@name})
+          $?.exitstatus == 0
+        end
+      
+        def install
+          IO.popen("sudo apt-get install --yes #{@name}") do |io|
+            while (line = io.gets) do
+              puts(line)
+            end
+          end
+        end
+      end
+      
+      class Command
+        def initialize(cmd)
+          @cmd = cmd
+        end
+      
+        def to_s
+          @cmd
+        end
+      
+        def installed?
+          false
+        end
+      
+        def run
+          @cmd.split("\n").each do |line|
+            IO.popen(line) do |io|
+              while (line = io.gets) do
+                puts(line)
+              end
+            end
+          end
+        end
+      end
+      
+      class NewGroup
+        def initialize(name, options)
+          @name = name
+          @install_flags = options.select{ |option| option == :system }.map{ |option| "--#{option.to_s}" }
+        end
+      
+        def to_s
+          @name
+        end
+      
+        def installed?
+          File.open("/etc/group") do |file|
+            file.each_line do |line|
+              return true if line.start_with?(@name)
+            end
+          end
+      
+          false
+        end
+      
+        def install
+          IO.popen("sudo addgroup --system docker") do |io|
+            while (line = io.gets) do
+              puts(line)
+            end
+          end
+        end
+      end
+      
+      class NewGroupMember
+        def initialize(user, group)
+          @user = user
+          @group = group
+        end
+      
+        def to_s
+          "#{@user} to #{@group}"
+        end
+      
+        def installed?
+          %x(groups #{@user}).partition(" : ").last.split(" ").include?(@group)
+        end
+      
+        def install
+          IO.popen("sudo adduser #{@user} #{@group}") do |io|
+            while (line = io.gets) do
+              puts(line)
+            end
+          end
+        end
+      end
+      
+      class Script
+        def initialize(source)
+          @source = source
+          @script = File.join(SCRIPTS_DIR, Digest::SHA1.hexdigest(source))
+        end
+      
+        def to_s
+          @source
+        end
+      
+        def installed?
+          File.exists?(@script)
+        end
+      
+        def install
+          URI.open(@source) do |source_file|
+            File.open(@script, "w") do |script_file|
+              script_file.write(source_file.read)
+            end
+          end
+      
+          IO.popen("bash #{@script}") do |io|
+            while (line = io.gets) do
+              puts(line)
+            end
+          end
+      
+        end
+      end
+      
+      class Directory
+        def initialize(directory)
+          @directory = directory
+        end
+      
+        def to_s
+          @directory
+        end
+      
+        def installed?
+          File.directory?(@directory)
+        end
+      
+        def install
+          FileUtils.mkdir_p(@directory)
+        end
+      end
+
+      class GitConf
+        def initialize(key, value)
+          @key = key
+          @value = value
+        end
+
+        def to_s
+          @key
+        end
+
+        def installed?
+          %x(git config --get #{@key}).strip == @value
+        end
+
+        def install
+          IO.popen("git config --global #{@key} #{@value}") do |io|
+            while (line = io.gets) do
+              puts(line)
+            end
+          end
+        end
+      end
+
+      class BashRC
+        def initialize(source)
+          @source = source
+          @hash = Digest::SHA1.hexdigest(source)
+        end
+
+        def to_s
+          "Appending some code to .bashrc"
+        end
+
+        def installed?
+          File.open(File.join(Dir.home, ".bashrc"), "r") do |file|
+            file.readlines.find{ |line| line.strip == "# WS_START #{@hash}" }
+          end
+        end
+
+        def install
+          File.open(File.join(Dir.home, ".bashrc"), "a") do |file|
+            file << <<~CODE
+
+              # WS_START #{@hash}
+
+              #{code.strip}
+
+              # WS_END #{@hash}
+            CODE
+          end
+        end
+
+        private
+
+        def code
+          if @source =~ URI::regexp
+            URI.open(@source){ |source_file| source_file.read }
+          else
+            @source
+          end
+        end
+      end
+
+      class Bin
+        def initialize(source, command = nil)
+          @source = source
+          @source_filename = source.split("/").last
+          @source_file_basename = @source_filename.partition(".").first
+          @command = command || @source_file_basename
+          @temp_destination = File.join("/tmp/", @source_filename)
+          @destination = File.join("/usr/local/bin/", @command)
+        end
+
+        def to_s
+          @source
+        end
+
+        def installed?
+          %x(which #{@command})
+          $?.exitstatus == 0
+        end
+
+        def install
+          URI.open(@source) do |source_file|
+            File.open(@temp_destination, "wb") do |script_file|
+              script_file.write(source_file.read)
+              script_file.chmod(0755)
+            end
+          end
+
+          if @source_filename.end_with?(".tar.gz")
+            unarchived_temp_destination = @temp_destination[0, @temp_destination.length - ".tar.gz".length]
+            %x(mkdir #{unarchived_temp_destination})
+            %x(tar xvf #{@temp_destination} -C #{unarchived_temp_destination})
+
+            # Attempt to find command file in unarchived folder
+            command_file = Dir[File.join(unarchived_temp_destination, "**", "*")].find do |file|
+              file.split("/").last == @command
+            end
+
+            %x(sudo cp #{command_file} #{@destination})
+          else
+            %x(sudo cp #{@temp_destination} #{@destination})
+          end
+        end
+      end
+
+      class Gsettings
+        def initialize(schema, key, value)
+          @schema = schema
+          @key = key
+          @value = value
+        end
+
+        def to_s
+          "#{@schema} #{@key} to #{@value}"
+        end
+
+        def installed?
+          %x(gsettings get #{@schema} #{@key}).strip == @value 
+        end
+
+        def install
+          %x(gsettings set #{@schema} #{@key} '#{@value}')
+        end
+      end
+  end
+end
